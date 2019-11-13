66c1c1cbf9b6d875dd0cc8eae0c801e5a91237db
Kubeusz <jakub_piwko@wp.pl>
2019-11-13 11:01:16 +0100

    Added Navbar Component

---
+++ led-wear/src/components/navBar.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <h1>LED WEAR</h1>
+        <nav>
+          <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <a
+              class="nav-item nav-link active"
+              id="nav-post-tab"
+              data-toggle="tab"
+              href="#nav-post"
+              role="tab"
+              aria-controls="nav-post"
+              aria-selected="true"
+            >
+              Post
+            </a>
+            <a
+              class="nav-item nav-link"
+              id="nav-animation-tab"
+              data-toggle="tab"
+              href="#nav-animation"
+              role="tab"
+              aria-controls="nav-animation"
+            >
+              Animation
+            </a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Nav;
