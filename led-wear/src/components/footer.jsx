55cc101b1021e4ab5f7cd319c0cbe7d2c9774bac
Kubeusz <jakub_piwko@wp.pl>
2019-11-16 14:07:57 +0100

    Color Picker Layout begun

---
+++ led-wear/src/components/footer.jsx
@@ -0,0 +1,8 @@
+<Figure className="shirt__figure">
+<Figure.Image
+  className="shirt__image"
+  alt="LED WEAR"
+  src={shirt}
+  fluid
+/>
+</Figure>
--- led-wear/src/components/main.jsx
+++ led-wear/src/components/main.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Figure, Tab, Nav, Form } from "react-bootstrap";
+import { Figure, Tab, Nav, Form, Container, Row, Col } from "react-bootstrap";
 import shirt from "../assets/images/shirt.jpg";
 import Settings from "./settings";
 import "./main.scss";
@@ -44,117 +44,150 @@ class Main extends Component {
             </div>
             <Tab.Content>
               <Tab.Pane eventKey="post-tab" title="Post-Tab">
-                <Figure className="shirt__figure">
-                  <Figure.Image
-                    className="shirt__image"
-                    alt="LED WEAR"
-                    src={shirt}
-                    fluid
-                  />
-                  <Form>
+                <div className="shirt__figure">
+                  <Form className="shirt__form">
                     <Form.Group controlId="formColorPickers">
                       <Form.Label>LEDs</Form.Label>
-                      <input type="color" id="left-collar" name="left collar" />
-                      <label for="left-collar" aria-label="left collar"></label>
-                      <input
-                        type="color"
-                        id="right-collar"
-                        name="right collar"
-                      />
-                      <label
-                        for="right-collar"
-                        aria-label="right collar"
-                      ></label>
-                      <input
-                        type="color"
-                        id="left-placket"
-                        name="left placket"
-                      />
-                      <label
-                        for="left-placket"
-                        aria-label="left placket"
-                      ></label>
-                      <input
-                        type="color"
-                        id="right-placket"
-                        name="right placket"
-                      />
-                      <label
-                        for="right-placket"
-                        aria-label="right placket"
-                      ></label>
-                      <input
-                        type="color"
-                        id="body-higher-left"
-                        name="body higher left"
-                      />
-                      <label
-                        for="body-higher-left"
-                        aria-label="body higher left"
-                      ></label>
-                      <input
-                        type="color"
-                        id="body-higher-right"
-                        name="body higher right"
-                      />
-                      <label
-                        for="body-higher-right"
-                        aria-label="body higher right"
-                      ></label>
-                      <input
-                        type="color"
-                        id="body-lower-left"
-                        name="body lower left"
-                      />
-                      <label
-                        for="body-lower-left"
-                        aria-label="body lower left"
-                      ></label>
-                      <input
-                        type="color"
-                        id="body-lower-right"
-                        name="body lower right"
-                      />
-                      <label
-                        for="body-lower-right"
-                        aria-label="body lower right"
-                      ></label>
-                      <input
-                        type="color"
-                        id="left-armhole"
-                        name="left armhole"
-                      />
-                      <label
-                        for="left-armhole"
-                        aria-label="left armhole"
-                      ></label>
-                      <input
-                        type="color"
-                        id="right-armhole"
-                        name="right armhole"
-                      />
-                      <label
-                        for="right-armhole"
-                        aria-label="right armhole"
-                      ></label>
-                      <input type="color" id="left-sleeve" name="left sleeve" />
-                      <label for="left-sleeve" aria-label="left sleeve"></label>
-                      <input
-                        type="color"
-                        id="right-sleeve"
-                        name="right sleeve"
-                      />
-                      <label
-                        for="right-sleeve"
-                        aria-label="right sleeve"
-                      ></label>
-                      <input type="color" id="left-cuff" name="left cuff" />
-                      <label for="left-cuff" aria-label="left cuff"></label>
-                      <input type="color" id="right-cuff" name="right cuff" />
-                      <label for="right-cuff" aria-label="right cuff"></label>
+                      <Container>
+                        <Row>
+                          <Col md={{ span: 2, offset: 1 }}>
+                            <input
+                              type="color"
+                              id="right-armhole"
+                              name="right armhole"
+                            />
+                            <label
+                              for="right-armhole"
+                              aria-label="right armhole"
+                            ></label>
+                          </Col>
+                          <Col md={{ span: 2, offset: 0 }}>
+                            <input
+                              type="color"
+                              id="right-collar"
+                              name="right collar"
+                            />
+                            <label
+                              for="right-collar"
+                              aria-label="right collar"
+                            ></label>
+                          </Col>
+                          <Col md={{ span: 2, offset: 2 }}>
+                            <input
+                              type="color"
+                              id="left-collar"
+                              name="left collar"
+                            />
+                            <label
+                              for="left-collar"
+                              aria-label="left collar"
+                            ></label>
+                          </Col>
+                          <Col md={{ span: 2, offset: 0 }}>
+                            <input
+                              type="color"
+                              id="left-armhole"
+                              name="left armhole"
+                            />
+                            <label
+                              for="left-armhole"
+                              aria-label="left armhole"
+                            ></label>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <input
+                            type="color"
+                            id="left-placket"
+                            name="left placket"
+                          />
+                          <label
+                            for="left-placket"
+                            aria-label="left placket"
+                          ></label>
+                          <input
+                            type="color"
+                            id="right-placket"
+                            name="right placket"
+                          />
+                          <label
+                            for="right-placket"
+                            aria-label="right placket"
+                          ></label>
+                        </Row>
+                        <Row>
+                          <input
+                            type="color"
+                            id="body-higher-left"
+                            name="body higher left"
+                          />
+                          <label
+                            for="body-higher-left"
+                            aria-label="body higher left"
+                          ></label>
+                          <input
+                            type="color"
+                            id="body-higher-right"
+                            name="body higher right"
+                          />
+                          <label
+                            for="body-higher-right"
+                            aria-label="body higher right"
+                          ></label>
+                          <input
+                            type="color"
+                            id="left-sleeve"
+                            name="left sleeve"
+                          />
+                          <label
+                            for="left-sleeve"
+                            aria-label="left sleeve"
+                          ></label>
+                          <input
+                            type="color"
+                            id="right-sleeve"
+                            name="right sleeve"
+                          />
+                          <label
+                            for="right-sleeve"
+                            aria-label="right sleeve"
+                          ></label>
+                        </Row>
+                        <Row>
+                          <input
+                            type="color"
+                            id="body-lower-left"
+                            name="body lower left"
+                          />
+                          <label
+                            for="body-lower-left"
+                            aria-label="body lower left"
+                          ></label>
+                          <input
+                            type="color"
+                            id="body-lower-right"
+                            name="body lower right"
+                          />
+                          <label
+                            for="body-lower-right"
+                            aria-label="body lower right"
+                          ></label>
+                          <input type="color" id="left-cuff" name="left cuff" />
+                          <label for="left-cuff" aria-label="left cuff"></label>
+                          <input
+                            type="color"
+                            id="right-cuff"
+                            name="right cuff"
+                          />
+                          <label
+                            for="right-cuff"
+                            aria-label="right cuff"
+                          ></label>
+                        </Row>
+                      </Container>
                     </Form.Group>
                   </Form>
-                </Figure>
+                </div>
               </Tab.Pane>
               <Tab.Pane
                 eventKey="animation-tab"
