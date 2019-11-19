756ae943748b99e97769a994d36595c54bba470f
Kubeusz <jakub_piwko@wp.pl>
2019-11-19 18:50:50 +0100

    Changed Grid system to input-positions system.

--- led-wear/src/components/main.jsx
+++ led-wear/src/components/main.jsx
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
-import { Figure, Tab, Nav, Form, Container, Row, Col } from "react-bootstrap";
-import shirt from "../assets/images/shirt.jpg";
+import { Tab, Nav, Form } from "react-bootstrap";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import Settings from "./settings";
 import "./main.scss";
+
+const positions = [
+  {
+    name: "Cuff: Left",
+    x: 0.05,
+    y: 0.8
+  },
+  {
+    x: 0.05,
+    y: 0.5
+  }
+];
+
+function init() {
+  const shirt = document.querySelector("#shirt");
+  const colorPickers = positions.map(position => {
+    console.log(position);
+    const colorPicker = document.createElement("input");
+    colorPicker.type = "color";
+    // colorPicker.addEventListener('change', ...)
+    // ...
+    colorPicker.style.left = position.x * 100 + "%";
+    colorPicker.style.top = `${position.y * 100}%`;
+    colorPicker.name = `${position.description}`;
+    shirt.appendChild(colorPicker);
+    return colorPicker;
+  });
+  console.log("COLOR PICKERS:", colorPickers);
+}
 
 class Main extends Component {
   render() {
@@ -44,150 +73,7 @@ class Main extends Component {
             </div>
             <Tab.Content>
               <Tab.Pane eventKey="post-tab" title="Post-Tab">
-                <div className="shirt__figure">
-                  <Form className="shirt__form">
-                    <Form.Group controlId="formColorPickers">
-                      <Form.Label>LEDs</Form.Label>
-                      <Container>
-                        <Row>
-                          <Col md={{ span: 2, offset: 1 }}>
-                            <input
-                              type="color"
-                              id="right-armhole"
-                              name="right armhole"
-                            />
-                            <label
-                              for="right-armhole"
-                              aria-label="right armhole"
-                            ></label>
-                          </Col>
-                          <Col md={{ span: 2, offset: 0 }}>
-                            <input
-                              type="color"
-                              id="right-collar"
-                              name="right collar"
-                            />
-                            <label
-                              for="right-collar"
-                              aria-label="right collar"
-                            ></label>
-                          </Col>
-                          <Col md={{ span: 2, offset: 2 }}>
-                            <input
-                              type="color"
-                              id="left-collar"
-                              name="left collar"
-                            />
-                            <label
-                              for="left-collar"
-                              aria-label="left collar"
-                            ></label>
-                          </Col>
-                          <Col md={{ span: 2, offset: 0 }}>
-                            <input
-                              type="color"
-                              id="left-armhole"
-                              name="left armhole"
-                            />
-                            <label
-                              for="left-armhole"
-                              aria-label="left armhole"
-                            ></label>
-                          </Col>
-                        </Row>
-                        <Row>
-                          <input
-                            type="color"
-                            id="left-placket"
-                            name="left placket"
-                          />
-                          <label
-                            for="left-placket"
-                            aria-label="left placket"
-                          ></label>
-                          <input
-                            type="color"
-                            id="right-placket"
-                            name="right placket"
-                          />
-                          <label
-                            for="right-placket"
-                            aria-label="right placket"
-                          ></label>
-                        </Row>
-                        <Row>
-                          <input
-                            type="color"
-                            id="body-higher-left"
-                            name="body higher left"
-                          />
-                          <label
-                            for="body-higher-left"
-                            aria-label="body higher left"
-                          ></label>
-                          <input
-                            type="color"
-                            id="body-higher-right"
-                            name="body higher right"
-                          />
-                          <label
-                            for="body-higher-right"
-                            aria-label="body higher right"
-                          ></label>
-                          <input
-                            type="color"
-                            id="left-sleeve"
-                            name="left sleeve"
-                          />
-                          <label
-                            for="left-sleeve"
-                            aria-label="left sleeve"
-                          ></label>
-                          <input
-                            type="color"
-                            id="right-sleeve"
-                            name="right sleeve"
-                          />
-                          <label
-                            for="right-sleeve"
-                            aria-label="right sleeve"
-                          ></label>
-                        </Row>
-                        <Row>
-                          <input
-                            type="color"
-                            id="body-lower-left"
-                            name="body lower left"
-                          />
-                          <label
-                            for="body-lower-left"
-                            aria-label="body lower left"
-                          ></label>
-                          <input
-                            type="color"
-                            id="body-lower-right"
-                            name="body lower right"
-                          />
-                          <label
-                            for="body-lower-right"
-                            aria-label="body lower right"
-                          ></label>
-                          <input type="color" id="left-cuff" name="left cuff" />
-                          <label for="left-cuff" aria-label="left cuff"></label>
-                          <input
-                            type="color"
-                            id="right-cuff"
-                            name="right cuff"
-                          />
-                          <label
-                            for="right-cuff"
-                            aria-label="right cuff"
-                          ></label>
-                        </Row>
-                      </Container>
-                    </Form.Group>
-                  </Form>
-                </div>
+                <div className="shirt__figure" id="shirt"></div>
               </Tab.Pane>
               <Tab.Pane
                 eventKey="animation-tab"
@@ -202,3 +88,131 @@ class Main extends Component {
 }
 
 export default Main;
+
+/*
+<Form className="shirt__form">
+                    <Form.Group controlId="formColorPickers">
+                      <Form.Label>LEDs</Form.Label>
+                      <MDBContainer>
+                        <MDBRow>
+                          <MDBCol size="2" md="3" id="right-col">
+                            <Form.Control
+                              type="color"
+                              name="right armhole"
+                              label="Right Armhole"
+                            />
+                          </MDBCol>
+                          <MDBCol size="2" md="3" id="center-col">
+                            <Form.Control
+                              type="color"
+                              name="right collar"
+                              label="Right Collar"
+                            />
+                          </MDBCol>
+                          <MDBCol size="3" md="3" id="center-col">
+                            <Form.Control
+                              type="color"
+                              name="left collar"
+                              label="Left Collar"
+                            />
+                          </MDBCol>
+                          <MDBCol size="3" md="3" id="left-col">
+                            <Form.Control
+                              type="color"
+                              name="left armhole"
+                              label="Left Armhole"
+                            />
+                          </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                          <MDBCol size="2" md="3"></MDBCol>
+                          <MDBCol size="2" md="3" id="center-col">
+                            <Form.Control
+                              type="color"
+                              name="right placket"
+                              label="Right Placket"
+                            />
+                          </MDBCol>
+                          <MDBCol size="2" md="3" id="center-col">
+                            <Form.Control
+                              type="color"
+                              name="left placket"
+                              label="Left Placket"
+                            />
+                          </MDBCol>
+                          <MDBCol size="2" md="3"></MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                          <MDBCol size="2" md="3" id="left-col">
+                            <Form.Control
+                              type="color"
+                              name="right sleeve"
+                              label="Right Sleeve"
+                            />
+                          </MDBCol>
+                          <MDBCol size="2" md="3" id="center-col">
+                            <Form.Control
+                              type="color"
+                              name="body higher right"
+                              label="Body Higher Right"
+                            />
+                          </MDBCol>
+                          <MDBCol size="2" md="3" id="center-col">
+                            <Form.Control
+                              type="color"
+                              name="body higher left"
+                              label="Body Higher Left"
+                            />
+                          </MDBCol>
+                          <MDBCol size="2" md="3" id="right-col">
+                            <Form.Control
+                              type="color"
+                              name="left sleeve"
+                              label="Left Sleeve"
+                            />
+                          </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                          <MDBCol size="2" md="3" id="left-col">
+                            <Form.Control
+                              type="color"
+                              name="right cuff"
+                              label="Right Cuff"
+                            />
+                          </MDBCol>
+                          <MDBCol size="2" md="3" id="center-col">
+                            <Form.Control
+                              type="color"
+                              id="body-lower-right"
+                              name="body lower right"
+                            />
+                          </MDBCol>
+                          <MDBCol size="2" md="3" id="center-col">
+                            <input
+                              type="color"
+                              id="body-lower-left"
+                              name="body lower left"
+                            />
+                            <label
+                              htmlFor="body-lower-left"
+                              aria-label="body lower left"
+                            ></label>
+                          </MDBCol>
+                          <MDBCol size="2" md="3" id="right-col">
+                            <input
+                              type="color"
+                              id="left-cuff"
+                              name="left cuff"
+                            />
+                            <label
+                              htmlFor="left-cuff"
+                              aria-label="left cuff"
+                            ></label>
+                          </MDBCol>
+                        </MDBRow>
+                      </MDBContainer>
+                    </Form.Group>
+                  </Form>
+*/
+
+init();
